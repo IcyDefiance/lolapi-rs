f482f28fea97fb241f7a424e8743b9cb639badc7
IcyDefiance <icydefiance@gmail.com>
2017-11-21 10:36:42 -0500

    Move by_account out of folder

--- src/dto.rs
+++ src/dto.rs
@@ -611,14 +611,14 @@ pub struct ParticipantIdentity {
 #[derive(Clone, Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Player {
+	#[serde(default)] pub summoner_id: i64,
+	#[serde(default)] pub account_id: i64,
+	#[serde(default)] pub platform_id: String,
+	#[serde(default)] pub current_account_id: i64,
 	#[serde(default)] pub current_platform_id: String,
 	#[serde(default)] pub summoner_name: String,
 	#[serde(default)] pub match_history_uri: String,
-	#[serde(default)] pub platform_id: String,
-	#[serde(default)] pub current_account_id: i64,
 	#[serde(default)] pub profile_icon: i32,
-	#[serde(default)] pub summoner_id: i64,
-	#[serde(default)] pub account_id: i64,
 }
 
 #[derive(Clone, Debug, Deserialize)]
