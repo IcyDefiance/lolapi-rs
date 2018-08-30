7d5b1b29b91da926c1413cf4b51fb942c50df790
IcyDefiance <icydefiance@gmail.com>
2018-08-29 20:55:11 -0400

    Case-insensitive deserialize

--- src/dto/platform.rs
+++ src/dto/platform.rs
@@ -1,7 +1,7 @@
+use serde::{ Deserialize, Deserializer, Serialize, Serializer, de };
 use std::fmt;
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
-#[serde(rename_all = "lowercase")]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum Platform {
 	BR1,
 	EUN1,
@@ -34,5 +34,43 @@ impl fmt::Display for Platform {
 			Platform::RU => write!(f, "RU"),
 			Platform::PBE1 => write!(f, "PBE1"),
 		}
+	}
+}
+impl Serialize for Platform {
+	fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+		serializer.serialize_str(&self.to_string())
+	}
+}
+impl<'de> Deserialize<'de> for Platform {
+	fn deserialize<D: Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+		struct Visitor;
+		impl<'de> de::Visitor<'de> for Visitor {
+			type Value = Platform;
+
+			fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+				formatter.write_str("one of `BR1`, `EUN1`, `EUW1`, `JP1`, `KR`, `LA1`, `LA2`, `NA`, `NA1`, `OC1`, `TR1`, `RU`, `PBE1` (case insensitive)")
+			}
+
+			fn visit_str<E: de::Error>(self, value: &str) -> Result<Platform, E> {
+				match value.to_uppercase().as_ref() {
+					"BR1" => Ok(Platform::BR1),
+					"EUN1" => Ok(Platform::EUN1),
+					"EUW1" => Ok(Platform::EUW1),
+					"JP1" => Ok(Platform::JP1),
+					"KR" => Ok(Platform::KR),
+					"LA1" => Ok(Platform::LA1),
+					"LA2" => Ok(Platform::LA2),
+					"NA" => Ok(Platform::NA),
+					"NA1" => Ok(Platform::NA1),
+					"OC1" => Ok(Platform::OC1),
+					"TR1" => Ok(Platform::TR1),
+					"RU" => Ok(Platform::RU),
+					"PBE1" => Ok(Platform::PBE1),
+					_ => Err(E::custom(format!("unknown Platform value: {}", value))),
+				}
+			}
+		}
+
+		deserializer.deserialize_u64(Visitor)
 	}
 }
